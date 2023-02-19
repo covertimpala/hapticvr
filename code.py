77ff7bef2714730e773a6a7c9ff2ff90e5a79e4d
covertimpala <77851547+covertimpala@users.noreply.github.com>
2023-02-19 14:26:20 +0100

    Update code.py

--- code.py
+++ code.py
@@ -4,11 +4,13 @@ import adafruit_lis3mdl
 import neopixel
 import time
 import math
+from analogio import AnalogIn
 #import busio
 
 from adafruit_ble import BLERadio
 from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
 from adafruit_ble.services.nordic import UARTService
+
 
 #neopixel-setup
 pixels = neopixel.NeoPixel(board.NEOPIXEL, 1)
@@ -17,6 +19,15 @@ pixels[0] = (10, 0, 0)
 i2c = board.I2C()
 sensor = LSM6DS33(i2c)
 magnetometer = adafruit_lis3mdl.LIS3MDL(i2c)
+analog_in = {}
+analog_in[0] = AnalogIn(board.A0)
+analog_in[1] = AnalogIn(board.A1)
+analog_in[2] = AnalogIn(board.A2)
+analog_in[3] = AnalogIn(board.A3)
+analog_in[4] = AnalogIn(board.A4)
+
+def get_voltage(pin):
+    return (pin.value * 3.3) / 65536
 
 ble = BLERadio()
 ble.name = "Haptic-right"
@@ -60,10 +71,11 @@ while True:
             dt = s_2 - s
             pitch = alpha * (pitch + gy * dt) + (1 - alpha) * pitch
             roll = alpha * (roll + gx * dt) + (1 - alpha) * roll
-            #print(sensor.acceleration)
-            #print(sensor.gyro)
-            #print(magnetometer.magnetic)
-            #dat = a_x,a_y,a_z,g_x,g_y,g_z,m_x,m_y,m_z
             print("Pitch: ", pitch, "Roll: ", roll, "Magnetometer: ", (mx, my, mz))
+            f0 = round(get_voltage(analog_in[0]),2)
+            f1 = round(get_voltage(analog_in[1]),2)
+            f2 = round(get_voltage(analog_in[2]),2)
+            f3 = round(get_voltage(analog_in[3]),2)
+            f4 = round(get_voltage(analog_in[4]),2)
             time.sleep(0.01)
             #uart.write(str(ang).encode())
